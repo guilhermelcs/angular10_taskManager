345907c3819bb78cfbb5af1938354a59ed3b16e7
Guilherme Lucas <guilherme.evandf@gmail.com>
2020-07-28 21:16:00 -0300

    Model TarefaModel criada

---
+++ src/app/tarefas/shared/tarefa.model.ts
@@ -0,0 +1,7 @@
+export class Tarefa {
+    constructor( public id?: number,
+                 public title?: string,
+                 public descricao?: string,
+                 public concluido?: boolean,
+                ) {}
+}
